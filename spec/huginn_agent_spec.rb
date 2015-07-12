68dab59d18f3447daaaac4cf5ee0dca9f9da56a9
Darren Cauthon <darren@cauthon.com>
2015-07-12 17:39:46 -0500

    Get tests going.

---
+++ spec/huginn_agent_spec.rb
@@ -0,0 +1,9 @@
+require_relative 'spec_helper'
+
+describe HuginnAgent do
+
+  it "should exist" do
+    true.must_equal true
+  end
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,3 @@
+require_relative '../lib/huginn_agent'
+require 'minitest/autorun'
+require 'minitest/spec'
