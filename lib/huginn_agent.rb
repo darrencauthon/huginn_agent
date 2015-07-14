937c0cc8894891243e51ae2582d2d41f25bd7cf3
Darren Cauthon <darren@cauthon.com>
2015-07-14 07:09:21 -0500

    An agent will need to be able to declare an actual agent class.

--- lib/huginn_agent.rb
+++ lib/huginn_agent.rb
@@ -1,4 +1,9 @@
 require "huginn_agent/version"
 
 class HuginnAgent
+
+  def self.emit
+    eval "class ::#{self.to_s}Agent; end"
+  end
+
 end
--- spec/huginn_agent_spec.rb
+++ spec/huginn_agent_spec.rb
@@ -1,9 +1,25 @@
 require_relative 'spec_helper'
 
+class TheTest < HuginnAgent
+end
+
 describe HuginnAgent do
 
   it "should be a class" do
     HuginnAgent.new.is_a?(HuginnAgent).must_equal true
+  end
+
+  describe "emit" do
+
+    after do
+      Object.send(:remove_const, :TheTestAgent) if Object.constants.include?(:TheTestAgent)
+    end
+
+    it "should create an agent class" do
+      TheTest.emit
+      Object.constants.include?(:TheTestAgent).must_equal true
+    end
+
   end
 
 end
