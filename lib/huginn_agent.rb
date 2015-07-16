3eb2bd0249fbe53342340986f4b1c0e3670814c2
Darren Cauthon <darren@cauthon.com>
2015-07-16 07:03:25 -0500

    Working on the direct huginn app integration.

--- lib/huginn_agent.rb
+++ lib/huginn_agent.rb
@@ -1,17 +1,29 @@
+require 'active_support/inflector'
 require "huginn_agent/version"
 
 class HuginnAgent
+
+  def self.inherited type
+    @types ||= []
+    @types << type
+  end
+
+  def self.types
+    @types ||= []
+  end
+
+  def self.agent_types
+    types.map { |x| "#{x}Agent".constantize }
+  end
 
   def self.description
   end
 
   def self.emit
-    eval <<EOF
-    class ::#{self.to_s}Agent < Agent
-    end
-EOF
+    eval "class ::#{self.to_s}Agent < Agent; end"
+
     the_description = self.description
-    eval("::#{self.to_s}Agent").instance_eval do
+    "#{self.to_s}Agent".constantize.class_eval do
       description the_description
     end
   end
